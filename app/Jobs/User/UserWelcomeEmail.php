5f319c360b2fb6914e5c55c109b69bba4f25b1e1
JohnWeeks1 <jonathanweeks007@gmail.com>
2020-11-10 23:31:33 +0000

    Updated doc block information

--- app/Jobs/User/UserWelcomeEmail.php
+++ app/Jobs/User/UserWelcomeEmail.php
@@ -2,26 +2,34 @@
 
 namespace App\Jobs\User;
 
-use App\Mail\User\WelcomeEmailAfterRegistration;
+use App\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Mail\User\WelcomeEmailAfterRegistration;
 
 class UserWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    /**
+     * User details.
+     *
+     * @var $user
+     */
     public $user;
 
     /**
-     * TestMail constructor.
+     * UserWelcomeEmail constructor.
      *
      * @param $user
+     *
+     * @return void
      */
-    public function __construct($user)
+    public function __construct(User $user)
     {
         $this->user = $user;
     }
@@ -31,7 +39,7 @@ class UserWelcomeEmail implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(): void
     {
         Mail::to($this->user->email)
             ->send(new WelcomeEmailAfterRegistration($this->user));
